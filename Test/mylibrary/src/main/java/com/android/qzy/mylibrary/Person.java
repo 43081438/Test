2db0d1fb9d263b9c586e0880dbec074c58d8d099
qzy <43081438@qq.com>
2017-05-12 18:31:00 +0800

    version update

--- Test/mylibrary/src/main/java/com/android/qzy/mylibrary/Person.java
+++ Test/mylibrary/src/main/java/com/android/qzy/mylibrary/Person.java
@@ -4,9 +4,11 @@ package com.android.qzy.mylibrary;
 public class Person {
     public String name;
     public int age;
+    public String address;
 
-    public Person(String name,int age){
+    public Person(String name,int age,String address){
         this.name = name;
         this.age = age;
+        this.address = address;
     }
 }
