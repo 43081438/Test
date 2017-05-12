be90306699e1c01b4ad578b17817282aa2724249
qzy <43081438@qq.com>
2017-05-12 17:45:59 +0800

    add

---
+++ Test/mylibrary/src/main/java/com/android/qzy/mylibrary/Person.java
@@ -0,0 +1,13 @@
+package com.android.qzy.mylibrary;
+
+
+public class Person {
+
+    public String name;
+    public int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
